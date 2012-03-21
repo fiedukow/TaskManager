f08612df77dad4828b8ee44cccbea869cc6d3b43
Andrzej Fiedukowicz <fiedukow@gmail.com>
2012-03-21 22:13:55 +0100

    Main functionality of ZarzadcaZadan class is ready now. However it isnt singleton yet, it isnt connected with EvolException to handle FileExceptions and it has no header file with will be necessary.

--- src/ZarzadcaZadan.cpp
+++ src/ZarzadcaZadan.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include "EvolException.hpp"
+//#include "EvolException.hpp"
 #include <string>
 #include <fstream>
 
@@ -14,7 +14,8 @@ class ZarzadcaZadan
     time_type **czasy;
     int iloscMaszyn, iloscZadan;
 
-    ZarzadcaZadan( std::string fileName ) throw (evol::CannotOpenFileException)
+    public: 
+    ZarzadcaZadan( std::string fileName ) /*throw (evol::CannotOpenFileException)*/
     {
         std::ifstream fileToRead;
         fileToRead.open( fileName.c_str(), std::ios::in );
@@ -23,20 +24,29 @@ class ZarzadcaZadan
             fileToRead >> iloscMaszyn >> iloscZadan;
             czasy = new time_type*[ iloscMaszyn ];
             for ( int i = 0; i < iloscMaszyn; ++i )
-            {
+            {                
                 czasy[i] = new time_type[iloscZadan];
             }     
             for ( int i = 0; i < iloscMaszyn; ++i )
             {
-                for( int j = 0; i < iloscZadan; ++j )
+                for( int j = 0; j < iloscZadan; ++j )
                 {
-                    fileToRead >> czasy[i][j];
+                    if( fileToRead.good() )
+                    {               
+                        fileToRead >> czasy[i][j];
+                    }
+                    else
+                    {
+                        throw 2;
+                    }
                 }
             }        
         }
         else
         {
-            throw evol::CannotOpenFileException(fileName);
+            std::cout << "FAIL?" << std::endl;
+            throw 1;
+            /*throw evol::CannotOpenFileException(fileName);*/
         }  
     }
 
@@ -49,4 +59,44 @@ class ZarzadcaZadan
         delete[] czasy;
     }
 
+    int getIloscMaszyn()
+    {
+        return iloscMaszyn;
+    }
+    
+    int getIloscZadan()
+    {
+        return iloscZadan;
+    }
+
+    void print()
+    {
+        std::cout << "Maszyn: " << iloscMaszyn << std::endl;
+        std::cout << "Zadan:  " << iloscZadan << std::endl;
+        for ( int i = 0; i < iloscMaszyn; ++i )
+        {
+            for ( int j = 0; j < iloscZadan; ++j )
+                std::cout << czasy[ i ][ j ] << " ";
+            std::cout << std::endl;
+        }
+    }
+
+    time_type czasObrobki( int idMaszyny, int idZadania )
+    {
+        return czasy[idMaszyny][idZadania];
+    }
 };
+
+int main()
+{
+    try{
+        ZarzadcaZadan zz("zadania.dat");
+        zz.print();
+        std::cout << zz.czasObrobki( 2, 0 ) << std::endl ;
+    }
+    catch( int a )
+    {
+        std::cout << "O KURCZE :( " << a << std::endl;
+    }
+    return 0;
+}
