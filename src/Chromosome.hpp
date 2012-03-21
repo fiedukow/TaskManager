4be236d7e01ee2cb3ed8f1b6f244fcebae10dba9
Andrzej Fiedukowicz <fiedukow@yester.(none)>
2012-03-21 21:03:39 +0100

    First new class Zadanie. For now it has crossWith method and some constructors. It also blocks mutate method becouse it cannot be used on this level (it will be implemented in class derived from Subject)

--- src/Chromosome.hpp
+++ src/Chromosome.hpp
@@ -26,7 +26,7 @@ class Chromosome
      * @param chromosome - smart pointer to Chromosome which dereferences to Chromosome to crossover with.
      * @return smart pointer to Chromosome which represents product of crossover current chromosome with chromosome from argument, which is new Chromosome.
      */
-    virtual ChromosomePtr crossWith(ChromosomePtr toCross ) const = 0;
+    virtual ChromosomePtr crossWith( ChromosomePtr toCross ) const = 0;
 
     /**
      * Do the mutation (self-modification) of current Chromosome
---
+++ src/TaskManager.cpp
@@ -0,0 +1,47 @@
+#include "Population.hpp"
+#include "Chromosome.hpp"
+#include "EvolFunctions.hpp"
+#include "EvolException.hpp"
+#include <cmath>
+#include <iostream>
+#include "Observer.hpp"
+
+#define NOT_CONNECTED -1
+
+class Zadanie;
+using namespace evol;
+
+typedef std::shared_ptr< Zadanie > ZadaniePtr;
+
+class Zadanie : public Chromosome
+{
+    int idSelf;
+    int maszyna;
+    public:
+    Zadanie ( int selfid ) : idSelf (selfid), maszyna(NOT_CONNECTED)
+    {}
+    Zadanie ( int selfid, int maszyna_ ) : idSelf (selfid), maszyna( maszyna_ ) 
+    {}
+    Zadanie ( const Zadanie* toCopy )
+    {
+        this->idSelf = toCopy->idSelf;
+        this->maszyna = toCopy->maszyna;
+    }
+
+    virtual ChromosomePtr crossWith( ChromosomePtr toCross ) const
+    {
+        if( EvolFunctions::random() > 0.5 ) 
+        {
+            Zadanie* toCopy = EvolFunctions::ptr_cast<ChromosomePtr, Zadanie>(toCross);  
+            return ZadaniePtr( new Zadanie( toCops ) );
+        }
+        else
+        {
+            return ZadaniePtr( new Zadanie ( this ) );
+        }
+    }  
+    
+    /*we cant mutate chromosome without knowing anything about whole Subject*/
+    virtual void mutate( )  
+    { return; }
+};
